11c639230a18faa2d4225c3ba1d70277c30ef91a
Oscar Casarez <32660038+ocasarez@users.noreply.github.com>
2018-12-13 16:49:00 -0800

    SignUp.php completed, DB connected

--- signup.php
+++ signup.php
@@ -3,21 +3,46 @@
 <body>
 
 <?php 
-/*foreach($_POST as $element => $value)
-print( "<p>".$value."</p>");
-*/
+function showTable(&$userList){
+			
+	$table = "<table><tr><th>Firstname</th><th>Lastname</th></tr>";
+	if($userList->num_rows > 0){
+		while($rows = $userList->fetch_assoc()){
+			$table = $table . "<tr><td>" . $rows["firstName"] . "</td><td>" . $rows["lastName"] . "</td></tr>";
+		}
+	}
+	$table = $table . "</table>";
+	
+	print $table;
+}	
+
+
 $fname = $_POST["firstname"];
 $lname = $_POST["lastname"];
 $phone = $_POST["phone"];
 $email = $_POST["email"];
-$user = $_POST["username"];
+$username = $_POST["username"];
 $pass = $_POST["password"];
 
-print $fname;
+
 
 $mysqli = new mysqli("localhost","webphp","password","finaldb");
+if($mysqli->connect_errno){
+	printf("Connect failed: %s\n", $mysqli->connect_error);
+	exit();
+}
 
-$send = "INSERT INTO auth(username, password, firstName, lastName, email, phone) VALUES ($user, $pass, $fname, $lname, $email, $phone)";
+//Insert data
+$query = $mysqli->query("INSERT INTO auth(username, password, firstName, lastName, email, phone) VALUES ('$username','$pass','$fname','$lname','$email','$phone')");
+if($query){//Sucess
+	print "<p>" . "Account successfully created" . "</p>";
+	$userList = $mysqli->query("SELECT firstName, lastName FROM auth");
+	showTable($userList);
+} else{//Error
+	print "<p>" . "Account creation not successful." . "</p>";
+}	
+
+$mysqli->close();
 ?>
 </body>
 </html>
